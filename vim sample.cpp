f3ca641b24c6433aec526bddcc147cdb47718b44
shriya2004 <113098964+shriya2004@users.noreply.github.com>
2022-12-19 15:43:38 +0530

    Create vim sample.cpp

---
+++ vim sample.cpp
@@ -0,0 +1,27 @@
+//Implicit type casting
+#include<iostream>
+using namespace std;
+int main()
+{
+    short x = 200;
+    int y;
+    y = x;
+    cout << " Implicit Type Casting " << endl;
+    cout << " The value of x: " << x << endl;
+    cout << " The value of y: " << y << endl;
+
+    int num = 20;
+    char ch = 'a';
+    int res = 20 + 'a';
+    cout << " Type casting char to int data type ('a' to 20): " << res << endl;
+
+    float val = num + 'A';
+    cout << " Type casting from int data to float type: " << val << endl;
+    return 0;
+ }
+~ output
+Implicit Type Casting 
+ The value of x: 200
+ The value of y: 200
+ Type casting char to int data type ('a' to 20): 117
+ Type casting from int data to float type: 85
